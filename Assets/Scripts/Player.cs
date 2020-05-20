758e3e8e590215725c3175ee18f9f43392e7ea27
jaygittings <jaygittings@yahoo.com>
2020-05-19 21:50:53 -0400

    Update
    Sandbox level.  Running, Jumping and Climbing.  Cinemachine cameras per state.  Multiple colliders on Player to handle friction against walls and ground.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,63 +1,107 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] Animator animator;
-    [SerializeField] float moveSpeed;
-    [SerializeField] float jumpForce;
+    const string GROUND_LAYER = "Ground";
+    const string LADDER_LAYER = "Ladders";
+
+    [SerializeField] Animator animator = null;
+    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] float climbSpeed = 3f;
 
     Rigidbody2D body;
     SpriteRenderer sprite;
-    bool isJumping;
+    BoxCollider2D playerCollider;
+    float distToGround;
+    bool isClimbing;
+    float startGravity;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
-        isJumping = false;
+        playerCollider = GetComponent<BoxCollider2D>();
+        isClimbing = false;
+        startGravity = body.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        Animate();
+        Run();
+        Jump();
+        Climb();
     }
 
-
-    private void Move()
+    private void Animate()
     {
         float controlThrow = Input.GetAxis("Horizontal"); //between +1 and -1
-        bool jumpButton = Input.GetButton("Fire1");
-        if (jumpButton && !isJumping)
+        if (IsOnLadder())
         {
-            //Debug.Log("jumped");
-            isJumping = true;
-            body.AddForce(new Vector2(0f, jumpForce));
+            Debug.Log("climb");
+            animator.SetTrigger("Climb");
         }
-
-        if(controlThrow != 0)
+        else if(controlThrow != 0)
         {
-            sprite.flipX = (controlThrow < 0);
-            var deltaX = controlThrow * Time.deltaTime * moveSpeed;
-            //var newX = transform.position.x + (deltaX);
-            //transform.position = new Vector2(newX, transform.position.y);
-            body.velocity = new Vector2(controlThrow * moveSpeed, body.velocity.y);
+            Debug.Log("walk");
             animator.SetTrigger("Walk");
         }
         else
         {
+            Debug.Log("idle");
             animator.SetTrigger("Idle");
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Run()
     {
-        if(collision.transform.position.y < transform.position.y)
+        float controlThrow = Input.GetAxis("Horizontal"); //between +1 and -1
+        if (controlThrow != 0)
         {
-            isJumping = false;
+            sprite.flipX = (controlThrow < 0);
+            body.velocity = new Vector2(controlThrow * moveSpeed, body.velocity.y);
         }
     }
-}
+
+    private void Jump()
+    {
+        bool jumpButton = Input.GetButton("Fire1");
+        if (jumpButton && IsGrounded())
+        {
+            body.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void Climb()
+    {
+        if (IsOnLadder())
+        {
+            float controlThrow = Input.GetAxis("Vertical"); //between +1 and -1
+            body.velocity = new Vector2(body.velocity.x, controlThrow * climbSpeed);
+            body.gravityScale = 0;
+        }
+        else
+        {
+            body.gravityScale = startGravity;
+        }
+    }
+
+    private bool IsOnLadder()
+    {
+        return playerCollider.IsTouchingLayers(LayerMask.GetMask(LADDER_LAYER));
+    }
+
+    private bool IsGrounded()
+    {
+        var distToGround = playerCollider.bounds.extents.y;
+        //Debug.DrawRay((Vector2)(transform.position), Vector2.down, Color.green);
+        return Physics2D.Raycast((Vector2)(transform.position), Vector2.down, distToGround + 0.5f, LayerMask.GetMask(GROUND_LAYER));
+    }
+
+ }
